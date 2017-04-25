092d50f789e0abe942c4f476d459f6fadb5645b3
collo <qollomcbizzy64@gmail.com>
2017-04-25 12:16:12 +0300

    add the function to reverse string

--- jscript/script.js
+++ jscript/script.js
@@ -3,3 +3,13 @@ var firlast=function(sentence){
   return sentence.charAt(0)+sentence.slice(-1);
 };
 alert(firlast(sentence));
+var firlast=function(sentence){
+  return sentence.slice(-1)+sentence.charAt(0);
+};
+alert(firlast(sentence));
+/*to reverse long string use this
+function reverseString(str) {
+    return (str === '') ? '' : reverseString(str.substr(1)) + str.charAt(0);
+}
+reverseString('dwayne');
+*/
